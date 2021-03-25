1afd23e4df3e22842d6a007b814adfa0e5ea7a8b
YuuJining <yujin092444@gmail.com>
2021-03-25 16:03:59 +0900

    2021/03/25 _app.js생성

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppLayout from '../components/Applayout';
+import Head from 'next/head';
 
 const Profile = () => {
     return (
-        <AppLayout>
-            <div>내정보 페이지</div>
-        </AppLayout>
+        <>
+            <Head>
+                <title>내 프로필 | NodeBird</title>
+            </Head>
+            <AppLayout>
+                <div>내정보 페이지</div>
+            </AppLayout>
+        </>
     );
     
 };
